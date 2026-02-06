5df71710156d97cdc473db58201ff212b5ffdd7f
surya <suryawanabakti18@gmail.com>
2026-02-07 03:29:09 +0800

    TEST

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   server: {
     proxy: {
       '^/(api|sanctum|login|logout|storage)': {
-        target: 'http://bankdatapppds.test',
+        target: 'https://datapppds2.med.unhas.ac.id',
         changeOrigin: true,
         secure: false,
-        cookieDomainRewrite: "bankdatapppds.test",
+        cookieDomainRewrite: "datapppds2.med.unhas.ac.id",
         bypass: (req) => {
           if (req.method === 'GET' && req.headers.accept?.includes('text/html')) {
             return '/index.html';
